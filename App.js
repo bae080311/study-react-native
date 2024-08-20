6be2f41e826e8a761a146f08f581fce49b14fdb4
bae080311 <s24024@gsm.hs.kr>
2024-08-20 19:55:13 +0900

    feat: temp, description, 지역 디자인

--- App.js
+++ App.js
@@ -8,7 +8,12 @@ export default function App() {
       <View style={styles.city}>
         <Text style={styles.cityname}>서울</Text>
       </View>
-      <View style={styles.weather}></View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -29,5 +34,17 @@ const styles = StyleSheet.create({
   },
   weather: {
     flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
   },
 });
